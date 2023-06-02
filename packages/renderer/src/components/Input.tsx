24fac586773943fb3454d794ee2652b8e181b6e0
dga <dima737.99@mail.ru>
2023-06-02 10:10:50 +0300

    update fix

--- packages/renderer/src/components/Input.tsx
+++ packages/renderer/src/components/Input.tsx
@@ -25,7 +25,7 @@ export const Input: Component<props> = ({ name, id, title, placeholder, onInput,
                     </div>
                 </Show>
 
-                <input ref={ref} onInput={onInput} type="text" name={name} id={id} class="block w-full rounded-md border-0 py-1.5 pl-1.5 pr-0 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder={placeholder} value={value || null} />
+                <input ref={ref} onInput={onInput} type="text" name={name} id={id} class="block w-full rounded-md border-0 py-1.5 pl-1.5 pr-0 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder={placeholder} value={value || null as any} />
             </div>
         </>
     )
